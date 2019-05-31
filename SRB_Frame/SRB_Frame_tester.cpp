fa09a78678e5c716c261d94f88e2b90e6503b703
lee8871_ubuntu <lee8871@126.com>
2019-05-31 10:32:14 +0800

    进行测试，速度正常 测试稍微修改了程序。

--- SRB_Frame/SRB_Frame_tester.cpp
+++ SRB_Frame/SRB_Frame_tester.cpp
@@ -1,6 +1,6 @@
 ﻿// SRB_Frame.cpp: 定义应用程序的入口点。
 //
-#include "SRB-Frame.h"
+#include "SRB_Frame_tester.h"
 #include "UsbToSrb.h"
 #include <time.h>
 
@@ -14,7 +14,7 @@ string timeToString(time_t tim)
 	strftime(str, sizeof(str), "%Y-%m-%d %H:%M:%S", localtime(&tim));
 	return str;
 }
-void check(const char *dscrp, int report_mini_value);
+__suseconds_t check(const char *dscrp, int report_mini_value);
 void beginCheck();
 #ifdef WINDOW_86
 LARGE_INTEGER bgn_time;
@@ -26,11 +26,12 @@ void beginCheck()
 	QueryPerformanceFrequency(&cpu_freq);
 	QueryPerformanceCounter(&bgn_time);
 }
-void check(const char *dscrp, int report_mini_value = 0)
+int check(const char *dscrp, int report_mini_value = 0)
 {
 	QueryPerformanceCounter(&end_time);
 	cout << dscrp << (end_time.QuadPart - bgn_time.QuadPart)*1000.0/(cpu_freq.QuadPart) << endl;
 	QueryPerformanceCounter(&bgn_time);
+	retunr 0;
 
 }
 #endif
@@ -38,42 +39,57 @@ void check(const char *dscrp, int report_mini_value = 0)
 struct timeval bgn_time;
 struct timeval end_time;
 
+ #include <sched.h>
+ #include <unistd.h>
+void setPriority(){      
+	struct sched_param param; 
+	int maxpri; 
+	maxpri = sched_get_priority_max(SCHED_FIFO);
+    param.sched_priority = maxpri; 
+	if (sched_setscheduler(getpid(), SCHED_FIFO, &param) == -1) {
+		cout << "priority set fail" << endl;
+	}
+	else{
+		cout << "priority set done" << endl;
+	}	
+}
 void beginCheck()
 {
-	gettimeofday(&bgn_time,null);
+	while(0 != gettimeofday(&bgn_time,null));
 }
-void check(const char *dscrp, int report_mini_value=0)
+__suseconds_t check(const char *dscrp, int report_mini_value=0)
 {
+	while(0 != gettimeofday(&end_time,null));
 	__suseconds_t diff = end_time.tv_usec - bgn_time.tv_usec;
-	gettimeofday(&end_time,null);
-	if(diff>report_mini_value){
-		cout << dscrp << end_time.tv_usec - bgn_time.tv_usec << endl;
+	if(diff<0){
+		diff+=1000000;
 	}
-	gettimeofday(&bgn_time,null);
+	if(diff>report_mini_value){
+		cout << dscrp << diff << endl;
+	}
+	while(0 != gettimeofday(&bgn_time,null));
+	return diff;
 }
 #endif
 
 UsbToSrb bus;
-const int TEST_PKG_NUM = 2000*100;
+const int TEST_PKG_NUM = 1000000;
 
 
-int main()
-{
+int main(int argc,char *argv[]){
+	setPriority();
 	BaseNode node;
 	node.addr = 10;
 	int16 speed = 0;
 	int temp;
 	cout << "try Open Port:" << bus.openUsbByName("USB-TEST-BED")<< endl;
 	time_t begin_time;	time(&begin_time);
-
-
-
-
-
 	cout << "Test send begin at "<<timeToString(begin_time)<<endl;
-	cout << TEST_PKG_NUM << " access is doing. "<< endl;
+	cout << TEST_PKG_NUM << " accessing is doing. "<< endl;
 	UsbAccess *access;
 	beginCheck();
+	long long int totle_send_time_us = 0;
+	double last;
 	for (int i = 0;i < TEST_PKG_NUM;i++) {
 		access = new UsbAccess(&node);
 		sSrbPkg* send_pkg = access->getSendPkg(); {
@@ -89,23 +105,20 @@ int main()
 		if (speed >= 0x1000) {
 			speed = 0;
 		}
-		check("\t\t\t\tcalculte time: ",-1);
-
+		check("calculate:",300);
 		bus.addAccess(access);
 		bus.doAccess();
-		check("send time: ",-1);
-
-
-
+		totle_send_time_us += check("send time >300 :",300);
 		delete access;
 	}
-
 	time_t end_time;	time(&end_time);
 	cout << "Test send end at " << timeToString(end_time) << endl;
-	cout << "Cost " <<(end_time-begin_time)<<" S"<< endl;
-	cout << "Cost " <<(end_time-begin_time)*1000.0 /(TEST_PKG_NUM)<<" mS / Access"<< endl;
+	cout << "It cost " <<end_time-begin_time<<"(s) at all."<< endl;
+	cout << "accessing time avariage is " <<totle_send_time_us/(TEST_PKG_NUM)<<"(us)."<< endl;
 
 
+#ifdef WINDOW_86
 	system("PAUSE");
+#endif
 	return 0;
 }

1457ad5b4c6570dce9ffd9a924b9b7065e1153d1
lee_Ubuntu <lee8871@126.com>
2019-07-22 17:22:00 +0800

    Add argument for accesses in each test

--- raspi-test/test_200.bash
+++ raspi-test/test_200.bash
@@ -1,6 +1,14 @@
 #!/bin/bash
-
 echo "Now test 200 times to edel-buzh"
+
+re='^[0-9]+$'
+if ! [[ $1 =~ $re ]] ; then
+   echo "Accesses in each test is not set, set to 10000"
+	 ACS_IN_EACH="10000"
+else
+   echo "Accesses in each set to $1"
+	 ACS_IN_EACH=$1
+fi
 
 TIME=$(date "+%Y%m%d-%H-%M-%S")
 for i in {1..201}
@@ -9,6 +17,6 @@ do
 	echo ""
 	echo "=========================================="
 	echo "test NO. $i :"
-	~/srb_ws/build/SRB_Frame_test/SRB_Frame_tester -BEdel-Baade -L~/srb_report/srb-log-edel${TIME}.v -SEdel -A2000
+	~/srb_ws/SRB-Frame-lib/build/SRB_Frame_test/SRB_Frame_tester -BEdel-Baade -L~/srb_report/srb-log-edel${TIME}.v -SEdel -A${ACS_IN_EACH}
 	sleep 0.1
 done

9aed53abfb35af7288ab5433c96d06885eb99c25
lee8871 <lee8871@126.com>
2019-07-29 18:40:21 +0800

    添加了字符串哈希并且简单（而不全面的）测试

---
+++ support/stringHash.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "lee.h"
+#include <iostream>
+namespace srb {
+	constexpr unsigned int getId(const char * str) {
+		int i = 0;
+		unsigned int id = 0;
+		unsigned int id_temp = 1;
+		const int mod = 1610612741;
+		unsigned char mapped = 0;
+		while (1) {
+			if (str[i] == 0) { return (id^id_temp); }
+			//map char form ASCII to quick mapping table str shold in lower case letters 				
+			mapped = (unsigned char)(str[i] - 'a' + 1);
+			if (mapped > 0x31) {
+				mapped = (27 + mapped % 5);
+			}
+			id_temp ^= ((unsigned int)mapped) << (1 + (i % 6) * 5);
+			i++;
+			if ((i % 6) == 0) {
+				id ^= id_temp;
+				id *= mod;
+				id_temp = 1;
+			}
+		}
+	}
+};
---
+++ test_hash/test_hash.cpp
@@ -0,0 +1,25 @@
+﻿#include <string.h>
+#include <stdio.h>
+
+#include "stringHash.h"
+
+using namespace std;
+using namespace srb;
+int main(int argc, char *argv[]) {
+	char cmd[20];
+	char argument_str[256];
+	char c;
+	while (1)	{
+		cmd[0] = 0;
+		argument_str[0] = 0;
+		printf(">");
+		scanf("%20s%[^\n]%c", cmd, argument_str, &c);
+		switch (getId(cmd)) {
+		case getId("hello"):
+			printf("Hi, %s\n",argument_str);
+			break;
+		default:
+			printf("input str is [%s],Id is %u\n", cmd, getId(cmd));
+		}
+	}
+}
